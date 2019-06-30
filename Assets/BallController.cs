0cf5cfe6efa620f25ef8a5aeb1ab15437e9e4559
Kenji Tsubota <kenji.tsubota@scsk.jp>
2019-06-30 20:05:22 +0900

    Lesson5課題

---
+++ Assets/BallController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	// ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	// ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+
+	// 得点を表示するテキスト
+	private GameObject pointsText;
+
+	// 得点
+	private int points = 0;
+
+	// Use this for initialization
+	void Start () {
+		// シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+
+		// シーン中のPointsTextオブジェクトを取得
+		this.pointsText = GameObject.Find("PointsText");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			// GameoverTextにゲームオーバを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}
+
+		// PointsTextに得点を表示
+		this.pointsText.GetComponent<Text> ().text = this.points.ToString() + " pt";
+	}
+
+	// 衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision other) {
+		// タグによって加算する点数を変える
+		if (other.gameObject.tag == "SmallStarTag") {
+			this.points += 10;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			this.points += 100;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			this.points += 25;
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			this.points += 500;
+		}
+	}
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour {
+	// HingeJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	// 初期の傾き
+	private float defaultAngle = 20;
+	// 弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		// HingeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		// フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// タッチされているかチェック
+		if (Input.touchCount > 0) {
+
+			// タッチ情報の取得
+			foreach (Touch touch in Input.touches) {
+
+				// タッチされた位置情報を取得
+				Vector2 newVec = new Vector2 (touch.position.x, touch.position.y);
+
+				// 画面左をタッチした時左フリッパーを動かす
+				if (touch.phase == TouchPhase.Began && newVec.x <= 375 && tag == "LeftFripperTag") {
+					SetAngle (this.flickAngle);
+				}
+				// 画面右をタッチした時右フリッパーを動かす
+				else if (touch.phase == TouchPhase.Began && newVec.x >= 375 && tag == "RightFripperTag") {
+					SetAngle (this.flickAngle);
+				}
+				// 画面左タッチが離された時フリッパーを元に戻す
+				else if (touch.phase == TouchPhase.Ended && newVec.x <= 375 && tag == "LeftFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+				// 画面右タッチが離された時フリッパーを元に戻す
+				else if (touch.phase == TouchPhase.Ended && newVec.x >= 375 && tag == "RightFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+			}		
+		// 矢印キー操作
+		} else {
+			// 左矢印キーを押した時左フリッパーを動かす
+			if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+				SetAngle (this.flickAngle);
+			}
+			// 右矢印キーを押した時右フリッパーを動かす
+			if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
+				SetAngle (this.flickAngle);
+			}
+
+			// 矢印キー離された時フリッパーを元に戻す
+			if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+				SetAngle (this.defaultAngle);
+			}
+			if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+				SetAngle (this.defaultAngle);
+			}
+		}
+	}
+
+	// フリッパーの傾きを設定
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+}
