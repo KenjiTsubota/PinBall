17e480f66b90afa28b8e3fbb5f0886af1cf9fe29
Kenji Tsubota <kenji.tsubota@scsk.jp>
2019-07-01 15:38:10 +0900

    得点表示する関数をUpdate()からOnCollisionEnter()に修正

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -14,7 +14,7 @@ public class BallController : MonoBehaviour {
 	private GameObject pointsText;
 
 	// 得点
-	private int points = 0;
+	private int point = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +23,7 @@ public class BallController : MonoBehaviour {
 
 		// シーン中のPointsTextオブジェクトを取得
 		this.pointsText = GameObject.Find("PointsText");
+
 	}
 
 	// Update is called once per frame
@@ -32,22 +33,23 @@ public class BallController : MonoBehaviour {
 			// GameoverTextにゲームオーバを表示
 			this.gameoverText.GetComponent<Text> ().text = "Game Over";
 		}
-
-		// PointsTextに得点を表示
-		this.pointsText.GetComponent<Text> ().text = this.points.ToString() + " pt";
 	}
 
 	// 衝突時に呼ばれる関数
 	void OnCollisionEnter(Collision other) {
 		// タグによって加算する点数を変える
 		if (other.gameObject.tag == "SmallStarTag") {
-			this.points += 10;
+			this.point += 10;
 		} else if (other.gameObject.tag == "LargeStarTag") {
-			this.points += 100;
+			this.point += 100;
 		} else if (other.gameObject.tag == "SmallCloudTag") {
-			this.points += 25;
+			this.point += 25;
 		} else if (other.gameObject.tag == "LargeCloudTag") {
-			this.points += 500;
+			this.point += 500;
 		}
+
+		// PointsTextに得点を表示
+		this.pointsText.GetComponent<Text> ().text = this.point.ToString() + " pt";
+
 	}
 }
